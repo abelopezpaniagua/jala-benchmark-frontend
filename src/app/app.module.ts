c501a23c719c868007d1b53e7991a4afc3fd17ab
Abel Lopez Paniagua <abel.lopez@jala-foundation.org>
2021-11-16 20:01:06 -0400

    install angular material-ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTableModule } from "@angular/material/table";
 
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { CategoriesListComponent } from './pages/categories-list/categories-list.component';
@@ -15,8 +18,10 @@ import { CategoriesListComponent } from './pages/categories-list/categories-list
   ],
   imports: [
     BrowserModule,
-    NgbModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
