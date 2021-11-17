1b081186736bdb700f84eaddf6230c8b76a99e9b
Abel Lopez Paniagua <abel.lopez@jala-foundation.org>
2021-11-16 20:01:56 -0400

    implement product list

--- src/app/pages/product-list/product-list.component.ts
+++ src/app/pages/product-list/product-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductService } from './../../core/services/product.service';
+import { ProductsDataSource } from './../../core/services/products.datasource';
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  dataSource!: ProductsDataSource;
+  displayedColumns = ["code", "name", "price"];
+
+  constructor(private _productService: ProductService) { }
 
   ngOnInit(): void {
+    this.populateProducts();
   }
 
+  public populateProducts() {
+    this.dataSource = new ProductsDataSource(this._productService);
+    this.dataSource.loadProducts('', 1, 3);
+  }
 }
